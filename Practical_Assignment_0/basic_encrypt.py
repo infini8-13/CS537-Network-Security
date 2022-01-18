f2d09432ba1dbe2ec07e5e1e4d499302e4e58240
infinite <ln.saaswath.eee19@itbhu.ac.in>
2022-01-18 20:03:40 +0530

    caesar

--- Practical_Assignment_0/basic_encrypt.py
+++ Practical_Assignment_0/basic_encrypt.py
@@ -1,45 +1,31 @@
 import streamlit as st
-import string
+
 
 st.set_page_config(
-     page_title="Encryption by Saaswath",
-     page_icon="🔏",
-     layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'About': "## Substitution Cipher. \nWith ❤ by *Saaswath*"
-     }
- )
-enc_map = {}
-dec_map = {}
-k = 5
-alphabet = string.ascii_letters
-
-
-# Encryption mapping
-for i in range(len(alphabet)):
-    enc_map[alphabet[i]] = alphabet[(i + k) % len(alphabet)]
-enc_map.update({' ': ' '})
-
-
-# Decryption mapping
-def inv_map():
-    for enc, dec in enc_map.items():
-        dec_map[dec] = enc
+    page_title="Encryption by Saaswath",
+    page_icon="🔏",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "## Caesar Cipher. \nWith ❤ by *Saaswath*"
+    }
+)
 
 
 def encrypt(pt):
     """
-    Encrypts the text based on the substitution encryption map
+    Encrypts the text, based on the caesar cipher scheme with disposition of 12
     :param pt: Plaintext input
     :return ct: Ciphertext output
     """
-
-    ct = ''
+    ct = ""
     try:
-        for p in pt:
-            tmp = enc_map[p]
-            ct += tmp
+        for i in range(len(pt)):
+            char = pt[i]
+            if char.isupper():
+                ct += chr((ord(char) - 53) % 26 + 65)
+            elif char.islower():
+                ct += chr((ord(char) - 85) % 26 + 97)
         return ct
     except KeyError:
         st.warning("Character not included in encryption scheme")
@@ -48,23 +34,26 @@ def encrypt(pt):
 
 def decrypt(ct):
     """
-    Decrypts the text, based on the substitution decryption map
+    Decrypts the text, based on the caesar cipher scheme with disposition of 12
     :param ct: Ciphertext input
     :return pt: Plaintext output
     """
-    inv_map()
     pt = ""
     try:
-        for c in ct:
-            tmp = dec_map[c]
-            pt += tmp
+        for i in range(len(ct)):
+            char = ct[i]
+            if char.isupper():
+                pt += chr((ord(char) - 51) % 26 + 65)
+            elif char.islower():
+                pt += chr((ord(char) - 83) % 26 + 97)
         return pt
+
     except KeyError:
         st.warning("Character not included in encryption scheme")
         return ct
 
 
-st.title('Substitution Cipher Tryout')
+st.title('Caesar Cipher - With a Shift of 13')
 st.markdown('### by Saaswath')
 in_txt = st.text_input('Your Message', placeholder="Enter text to encrypt/decrypt")
 crypt_opt = st.radio(
@@ -77,3 +66,4 @@ if in_txt != "":
     else:
         st.write("Here is the decipher:")
         st.markdown('**' + decrypt(in_txt) + '**')
+
